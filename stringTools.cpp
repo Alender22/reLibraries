ece9d65816993a870fe56430a0e891544e313b1d
Lucas A. Ethington <lucas@ethington.de>
2023-04-08 19:30:58 -0500

    added replace functions to string tools, as well as documentation

--- stringTools.cpp
+++ stringTools.cpp
@@ -1,5 +1,18 @@
 #include "stringTools.h"
 
+//Make empty tokens list
+//make empty token string
+//loop through original string.
+//if current index is start of substring matching splitMarker
+//  push token to back of list
+//  increment i by lengthOfSplitMarker - 1
+//  set token to empty string 
+//otherwise
+//  append current char to end of token string
+//check if current i does not allow for further substring matches
+//  if so, append remainder of main string to token string 
+//  and append token string to tokens list
+//  return tokens list
 vector<string> splitStringAt(string toSplit, string splitAt)
 {
     vector<string> tokens;
@@ -32,4 +45,38 @@ vector<string> splitStringAt(string toSplit, char splitAt)
     string marker = "";
     marker += splitAt;
     return splitStringAt(toSplit, marker);
+}
+
+//split main string at replace string
+//append token to formatted string
+//for token in tokens
+//  if appended token is not the last token
+//    append replace string to formatted string
+//return formatted string
+string replaceSubtsring(string main, string replace, string newString)
+{
+    vector<string> tokens = splitStringAt(main, replace);
+    string formatted = "";
+
+    for(int i = 0; i < tokens.size(); i++)
+    {
+        formatted += tokens[i];
+        if(i < tokens.size() - 1)
+        {
+            formatted += newString;
+        }
+    }
+
+    return formatted;
+}
+
+template<typename a, typename b>
+string replaceSubtsring(string main, a replaceChars, b newChars)
+{
+    string replaceString = "";
+    replaceString += replaceChars;
+
+    string newString = "";
+    newString += newChars;
+    return replaceSubtsring(main, replaceString, newString);
 }
--- stringTools.h
+++ stringTools.h
@@ -8,7 +8,23 @@
 
 using namespace std;
 
+//Description: Takes a main string, and a marker string. 
+//  returns a vector of the substrings of the main strings,
+//  which were sepereated by the marker string
+//Pre: A string to split, and a string to split at
+//Post: A vector of the resulting substrings
 vector<string> splitStringAt(string toSplit, string splitAt);
+
+//Alias for splitStringAt(string, string) with splitAt char
 vector<string> splitStringAt(string toSplit, char splitAt);
+
+//Description: Replaces given substring with a new string
+//Pre:A string, subtring to replace, new substring
+//Post:string with the replaced substring
+string replaceSubtsring(string main, string replace, string newString);
+
+//Allows replaceSubtsring to be called with any combination of string and char for replace and newString
+template<typename a, typename b>
+string replaceSubtsring(string main, a replaceChar, b newString);
 
 #endif 
